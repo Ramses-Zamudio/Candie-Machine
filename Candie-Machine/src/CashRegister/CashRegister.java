2e4b2fc758a036d0982b94aa988afe5da227c8c7
bloos <bloos@Lap-Ram>
2026-03-08 23:35:40 -0600

    Class CashRegister complete

---
+++ Candie-Machine/src/CashRegister/CashRegister.java
@@ -0,0 +1,26 @@
+package CashRegister;
+
+public class CashRegister {
+	
+	private int cashOnHand;
+	
+	public CashRegister(){
+		cashOnHand = 500;
+	}
+	
+	public CashRegister(int cashIn) {
+		if (cashIn > 0)
+			cashOnHand = cashIn;
+		else 
+			cashOnHand = 500;
+	}
+	
+	public int currentBalance() {
+		return cashOnHand;
+	}
+	
+	public void acceptAmount(int amountIn) {
+		cashOnHand = cashOnHand + amountIn;
+	}
+	
+}
